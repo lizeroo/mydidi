0ccf941d5159a7f8d71b040292c813ab3b2e4db0
liwl <lizero.liwl@gmail.com>
2018-02-26 12:35:00 +0800

    Initial commit 项目开始

---
+++ app/src/main/java/com/liwl/mydidi/DiDiApp.java
@@ -0,0 +1,12 @@
+package com.liwl.mydidi;
+
+import android.app.Application;
+
+/**
+ * @Desc:
+ * @author: liwl
+ * @date: 2018/2/26
+ * @version: v1.0
+ */
+public class DiDiApp extends Application {
+}
